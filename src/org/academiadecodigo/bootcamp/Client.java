88bed20e983fb11a923831f3008344eb27b7c6c3
ANAlogic <ana.raquel.carlos.carmo@hotmail.com>
2019-03-06 17:20:27 +0000

    multiple users interacting. Possibility to change nickname and quit

--- src/org/academiadecodigo/bootcamp/Client.java
+++ src/org/academiadecodigo/bootcamp/Client.java
@@ -3,16 +3,15 @@ package org.academiadecodigo.bootcamp;
 import java.io.*;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Client {
 
-    private final int PORT = Server.PORT;
     private final String HOSTNAME = "localhost";
 
     private Socket socket;
-    private String nickname;
-    private BufferedReader terminalIn;
-    private PrintWriter out;
+
 
     public static void main(String[] args) {                                                                    //thread 1
 
@@ -20,10 +19,6 @@ public class Client {
 
         try{
             client.connect();
-            client.setNickname();
-            while (true) {
-                client.startConversation();
-            }
         } catch (UnknownHostException ex1){
             System.out.println("Seems like that host doesn't exist. Please check it out.");
         } catch (IOException ex2){
@@ -33,57 +28,43 @@ public class Client {
 
 
     public void connect () throws UnknownHostException, IOException {
+
         socket = new Socket(HOSTNAME, Server.PORT);
-        Thread thread = new Thread(new MessageOut(socket));
-        thread.start();
-    }
 
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        BufferedReader terminalIn = new BufferedReader(new InputStreamReader(System.in));
 
-    public void setNickname () throws IOException {
-        terminalIn = new BufferedReader(new InputStreamReader(System.in));
-        System.out.print("Enter your nickname: ");
-        nickname = terminalIn.readLine();
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
 
-        //send nickname to server
-        out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
-        out.println(nickname + " logged in");
-        out.flush();
-    }
-
-
-    private void startConversation () throws IOException {
-
-        //message introduced by client and sent to server
-        System.out.print(nickname + ": ");
-        String message = terminalIn.readLine();
-        out.println(nickname + ": " + message);
-        out.flush();
-
-
-
-    }
-
-
-    private class MessageOut implements Runnable {                                                              //thread 2
-
-        private Socket socket;
-
-        public MessageOut (Socket socket){
-            this.socket = socket;
-        }
-
-
-        @Override
-        public void run(){
-            //messages received from server and printed to terminal
-            try {
-                while(true) {
-                    BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                    System.out.println("Server: " + in.readLine());
+        executorService.submit(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    while (true) {
+                        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        System.out.println(in.readLine());
+                    }
+                } catch (IOException e){
+                    System.out.println(e.getMessage());
                 }
-            }catch (IOException ex){
+
             }
+        });
+
+
+        while(true) {
+
+            String message = terminalIn.readLine();
+            out.println(message);
+
         }
+
+
+
+
     }
+
+
+
 
 }
--- src/org/academiadecodigo/bootcamp/Server.java
+++ src/org/academiadecodigo/bootcamp/Server.java
@@ -1,12 +1,9 @@
 package org.academiadecodigo.bootcamp;
 
-import com.sun.corba.se.spi.orbutil.threadpool.Work;
-
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -21,6 +18,7 @@ public class Server {
             Server server = new Server();
             while (true) {
                 server.connect();
+
             }
         } catch (IOException ex) {
         }
@@ -33,15 +31,30 @@ public class Server {
 
     public Server() throws IOException {
         serverSocket = new ServerSocket(PORT);
+        workers = new ArrayList<>();
     }
 
 
     public void connect() throws IOException {
         Socket socket = serverSocket.accept();
-        System.out.println("A new connection was established with " + socket.getInetAddress() + " at port " + socket.getPort());
         Worker worker = new Worker(socket);
+        workers.add(worker);
         ExecutorService thread = Executors.newCachedThreadPool();
         thread.submit(worker);
+    }
+
+
+    public void sendAll(String message) {
+        for (Worker w : workers) {
+            w.send(message);
+        }
+    }
+
+
+    private void listWorkers (){
+        for (Worker w: workers){
+            System.out.println(w.nickname);
+        }
     }
 
 
@@ -50,6 +63,9 @@ public class Server {
     public class Worker implements Runnable {
 
         private Socket socket;
+        private PrintWriter out;
+        private BufferedReader in;
+        private String nickname;
 
         public Worker(Socket socket) {
             this.socket = socket;
@@ -60,24 +76,58 @@ public class Server {
         public void run() {
 
             try {
-                PrintWriter out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);
-                out.println("Enter your nickname: ");
 
+                start();
+                String input = "";
 
-                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                String nickname = in.readLine();
-                out.println("WELCOME " + nickname + " :) ");
+                while (!(input = in.readLine()).equals("*quit")) {
 
-                System.out.println(nickname + " logged in");
-                while (socket.isBound()) {
-                    //out.println(nickname + ": ");
-                    System.out.println(nickname + ": " + in.readLine());
+                    if (input.equals("*change")) {
+                        nickname = in.readLine();
+                        out.println("Your nickname has been changed to " + nickname);
+                        continue;
+                    }
+
+                    String message = nickname + ": " + input;
+                    System.out.println(message);
+                    sendAll(message);
                 }
-            }catch (IOException ex){
 
+                closeConnection();
+
+            } catch (IOException ex) {
+                System.out.println(ex.getMessage());
             }
 
         }
+
+
+        private void start() throws IOException {
+            out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);
+            out.println("Enter your nickname: ");
+
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            nickname = in.readLine();
+
+            out.println("WELCOME " + nickname + " :) \n\n-- Commands: --\n*quit to exit conversation \n*change to change nickname\n");
+            System.out.println(nickname + " logged in (IPaddress: " + socket.getInetAddress() + ")");
+        }
+
+
+        private void send(String message) {
+            out.println(message);
+        }
+
+
+        private void closeConnection() throws IOException {
+            sendAll(nickname + " left the conversation");
+            System.out.println(nickname + " left the conversation!");
+
+            socket.close();
+            out.close();
+            in.close();
+        }
+
     }
 }
 
